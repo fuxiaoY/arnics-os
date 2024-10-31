d29c29ac2dafd628c0cef57c9e6aa7a92e660ad0
fuxiaoY <18280138746@163.com>
2024-10-31 16:54:41 +0800

    【文件整理】：放置到单独文件夹

---
+++ arnics-os/port/portInclude.h
@@ -0,0 +1,18 @@
+#ifndef _PORTINCLUDE_H
+#define _PORTINCLUDE_H
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "../Inc/projDefine.h"
+#include "../Inc/typedef.h"
+
+#include "delay.h"
+
+
+#ifdef __cplusplus
+}
+#endif
+#endif
