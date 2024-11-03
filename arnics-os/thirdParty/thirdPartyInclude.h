f00817eb74f9a7824c7a0a10dd0e71a881663b47
Arong <45900196+fuxiaoY@users.noreply.github.com>
2024-11-03 12:21:57 +0800

    【thirdParty】:增加flashDB

--- arnics-os/thirdParty/thirdPartyInclude.h
+++ arnics-os/thirdParty/thirdPartyInclude.h
@@ -10,8 +10,8 @@ extern "C" {
 #include "struct2json/inc/s2j.h"
 #include "sfud/inc/sfud.h"
 #include "fal/inc/fal.h"
-//需适配接口：
-//#include "../thirdPartyPort/FlashDB/inc/flashdb.h"
+#include "FlashDB/inc/flashdb.h"
+
 
 extern void thirdPartyInit(void);
 #ifdef __cplusplus
