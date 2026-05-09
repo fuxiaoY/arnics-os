d188a7cc5c67ac3b30ce9c286b2f4d3529daefef
Arong <45900196+fuxiaoY@users.noreply.github.com>
2026-05-10 00:47:52 +0800

    【include】:补充完整组件

--- arnics-os/Inc/include.h
+++ arnics-os/Inc/include.h
@@ -9,6 +9,16 @@ extern "C" {
 // 内核
 #include "core/coreInclude.h"
 #include "common/commonInclude.h"
+// RTOS
+#include "rtosInterface/rtosInclude.h"
+// 数据平台
+#include "dataPlat/dataPlatInclude.h"
+// 驱动层
+#include "drivers/driversInclude.h"
+// 接口
+#include "port/portInclude.h"
+// 三方组件
+#include "thirdParty/thirdPartyInclude.h"
 
 #ifdef __cplusplus
 }
