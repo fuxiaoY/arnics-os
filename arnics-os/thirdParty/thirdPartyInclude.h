120827468da119078ceb0202870c0618a645b499
Arong <45900196+fuxiaoY@users.noreply.github.com>
2026-05-13 21:14:01 +0800

    【thirdParty】:第三方库采用arnics根目录路径

--- arnics-os/thirdParty/thirdPartyInclude.h
+++ arnics-os/thirdParty/thirdPartyInclude.h
@@ -4,18 +4,18 @@
 extern "C" {
 #endif
 
-#include "unity/api_unity_def.h"
-#include "Assert/usr_assert.h"
-#include "ring/ring.h"
-#include "struct2json/inc/s2j.h"
-#include "modContact/mctInclude.h"
-#include "Runonce/Runonce.h"
-#include "rollDB/rolldbPort.h"
-#include "softMutex/soft_mutex.h"
-#include "uflog/uflog.h"
-// #include "sfud/inc/sfud.h"
-// #include "fal/inc/fal.h"
-// #include "upgrade/upgradeManager.h"
+#include "thirdParty/unity/api_unity_def.h"
+#include "thirdParty/Assert/usr_assert.h"
+#include "thirdParty/ring/ring.h"
+#include "thirdParty/struct2json/inc/s2j.h"
+#include "thirdParty/modContact/mctInclude.h"
+#include "thirdParty/Runonce/Runonce.h"
+#include "thirdParty/rollDB/rolldbPort.h"
+#include "thirdParty/softMutex/soft_mutex.h"
+#include "thirdParty/uflog/uflog.h"
+// #include "thirdParty/sfud/inc/sfud.h"
+// #include "thirdParty/fal/inc/fal.h"
+// #include "thirdParty/upgrade/upgradeManager.h"
 
 extern void thirdPartyInit(void);
 #ifdef __cplusplus
