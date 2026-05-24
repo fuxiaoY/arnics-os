147751b6601ec80d69df38ecf6bb50756283e60a
arong_yi <3221478971@qq.com>
2026-05-24 11:32:43 +0800

    【督察中心】：心跳监控

--- arnics-os/dePartment/centerGuard/guard_process.c
+++ arnics-os/dePartment/centerGuard/guard_process.c
@@ -7,12 +7,27 @@
 _SECTION( "._entry_guard_api")
 #endif
 
-
+void arnics_sys_tick_check(void)
+{
+    static uint32_t last_tick     = 0xff;
+    uint32_t     current_tick     = arnics_getTick();
+    if (current_tick == last_tick)
+    {
+        UFLOG_ERR("guard:arnics_systick is not changing, system clock may not be running!");
+    }
+    else 
+    {
+        last_tick = current_tick;
+    }
+}
 
 void guard_process()
 {
+
     while(1)
     {
+        // 检查 tick 在变化，表明时钟未失效
+        arnics_sys_tick_check();
         rtosThreadDelay(1000);
     }
 
